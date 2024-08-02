835a3ecc358e245e8e85ce1dc3d8108820f77e60
Ritesh <ritesh.mvteams@gmail.com>
2024-08-02 16:35:03 +0530

    add get user fact route

--- src/controllers/fact.controller.js
+++ src/controllers/fact.controller.js
@@ -17,8 +17,7 @@ const createFact = async (req, res, next) => {
 
     if (createdFact) {
       res.status(201).json({
-        message: 'created',
-        createdFact
+        message: 'created successfully'
       });
     } else {
       return next(createError('Error while creating the Fact', 400));
@@ -45,7 +44,23 @@ const getAllFacts = async (req, res, next) => {
     return next(createError('Error while getting the Facts', 500));
   }
 };
-const getUserFacts = async (req, res) => {
+
+const getUserFacts = async (req, res, next) => {
+  try {
+    const facts = await factModel
+      .find({ user: req.userId })
+      .populate({ path: 'user', select: 'name email role' })
+      .populate({ path: 'category', select: 'name' })
+      .select('-__v');
+    res.status(200).json({
+      data: facts,
+      count: facts?.length
+    });
+  } catch (error) {
+    console.log(error);
+    return next(createError('Error while getting the Facts', 500));
+  }
+
   res.status(200).json({
     message: 'Success'
   });
