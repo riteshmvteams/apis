9b6ddc9badbc015392b1fa3d1c8b6555bf1d54b2
Ritesh <ritesh.mvteams@gmail.com>
2024-08-05 11:47:06 +0530

    update post

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import connectDB from './config/db.js';
 
 const startServer = async () => {
   await connectDB();
-  const port = config.port;
+  const port = config.port || 4080;
   app.listen(port, () => {
     console.log(`server is listening on port: ${port}`);
   });
